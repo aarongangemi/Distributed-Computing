fb4ba73acf74a767110aa092de9c6a893e228904
aaron <aaron.gangers@gmail.com>
2020-03-08 14:50:49 +0800

    Clock Server commit

---
+++ ClockServer/TimeServer.cs
@@ -0,0 +1,25 @@
+﻿using ClockServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockServer
+{
+    class TimeServer
+    {
+        [Obsolete]
+        static void Main()
+        {
+            TcpServerChannel channel = new TcpServerChannel(1234);
+            ChannelServices.RegisterChannel(channel);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(Clock), "Clock", WellKnownObjectMode.SingleCall);
+            Console.WriteLine("Press enter to terminate");
+            Console.ReadLine();
+        }
+    }
+}
