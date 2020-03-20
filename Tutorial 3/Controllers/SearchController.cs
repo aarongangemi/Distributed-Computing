27f6ae04597862fe7eef40b3a227d4220a1b1100
aaron <aaron.gangers@gmail.com>
2020-03-20 12:41:51 +0800

    API functionality included

---
+++ Tutorial 3/Controllers/SearchController.cs
@@ -0,0 +1,38 @@
+﻿using Bis_GUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Tutorial_3.Models;
+
+namespace Tutorial_3.Controllers
+{
+    public class SearchController : ApiController
+    {
+        public DataIntermed Post([FromBody]SearchData value)
+        {
+            DataIntermed dataInter = new DataIntermed();
+            DataModel model = new DataModel();
+            int i;
+            uint acntNo, pin;
+            int bal;
+            string fname, lname;
+            for (i = 0; i < model.getNumEntries(); i++)
+            {
+                model.GetValuesForEntry(i, out acntNo, out pin, out bal, out fname, out lname);
+                if (lname.Equals(value.ToString()))
+                {
+                    dataInter.lname = lname;
+                    dataInter.acct = acntNo;
+                    dataInter.pin = pin;
+                    dataInter.bal = bal;
+                    dataInter.fname = fname;
+                    break;
+                }
+            }
+            return dataInter;
+        }
+    }
+}
---
+++ Tutorial 3/Controllers/WebAPIController.cs
@@ -0,0 +1,59 @@
+﻿using Bis_GUI;
+using Remoting_Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+using System.Web.Http;
+using Tutorial_3.Models;
+
+namespace Tutorial_3.Controllers
+{
+    public class WebAPIController : ApiController
+    {
+        //GET api/<controller>
+        DataModel model = new DataModel();
+        public int Get()
+        {
+            return model.getNumEntries();
+        }
+        
+        //GET api/<controller>/5
+        public DataIntermed Get(int id)
+        {
+            DataModel model = new DataModel();
+            DataIntermed dataIm = new DataIntermed();
+            uint acntNo, pin;
+            int balance;
+            string fname, lname;
+            model.GetValuesForEntry(id, out acntNo, out pin, out balance, out fname, out lname);
+            dataIm.acct = acntNo;
+            dataIm.pin = pin;
+            dataIm.bal = balance;
+            dataIm.fname = fname;
+            dataIm.lname = lname;
+            return dataIm;
+        }
+
+        //POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+
+        }
+        
+        //PUT api/<controller>/5
+        public void Put(int id,[FromBody]string value)
+        {
+
+        }
+
+        //DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+
+        }
+
+        
+    }
+}
