b1107624d11e4c71a273c41e9ea6cbb23ea0a47e
aaron <aaron.gangers@gmail.com>
2020-05-07 18:30:52 +0800

    Log error handling added

--- Bis-GUI/LogData.cs
+++ Bis-GUI/LogData.cs
@@ -14,71 +14,159 @@ namespace Bis_GUI
 
         public void logNumEntries(int noOfEntries)
         {
-            writer = new StreamWriter(path, append: true);
-            writer.WriteLine("Log file function: Starting application");
-            writer.WriteLine(noOfEntries + " results are in the database");
-            writer.Close();
+            try
+            {
+                writer = new StreamWriter(path, append: true);
+                writer.WriteLine("Log file function: Starting application");
+                writer.WriteLine(noOfEntries + " results are in the database");
+                writer.Close();
+            }
+            catch(DirectoryNotFoundException)
+            {
+                Console.WriteLine("Unable to log data");
+            }
+            catch(IOException)
+            {
+                Console.WriteLine("Unable to log data");
+            }
         }
 
         public void logSearch(DataIntermed dataInter)
         {
-            writer = new StreamWriter(path, append: true);
-            writer.WriteLine("Log file function: Search by lastname. ");
-            writer.WriteLine("Account: " + dataInter.acct +
-                " has searched for last name: " + dataInter.lname
-                + " and found a result for: " + dataInter.fname + " " +
-                dataInter.lname);
-            writer.Close();
+            try
+            {
+                writer = new StreamWriter(path, append: true);
+                writer.WriteLine("Log file function: Search by lastname. ");
+                writer.WriteLine("Account: " + dataInter.acct +
+                    " has searched for last name: " + dataInter.lname
+                    + " and found a result for: " + dataInter.fname + " " +
+                    dataInter.lname);
+                writer.Close();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Unable to log data");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Unable to log data");
+            }
         }
 
         public void logImageUpload(string filePath)
         {
-            writer = new StreamWriter(path, append: true);
-            writer.WriteLine("Log File Function: Upload profile image");
-            writer.WriteLine("Image upload from File Path: " + filePath);
-            writer.Close();
+            try
+            {
+                writer = new StreamWriter(path, append: true);
+                writer.WriteLine("Log File Function: Upload profile image");
+                writer.WriteLine("Image upload from File Path: " + filePath);
+                writer.Close();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Unable to log data");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Unable to log data");
+            }
         }
 
         public void logIndexSearch(DataIntermed dataInter)
         {
-            writer = new StreamWriter(path, append: true);
-            writer.WriteLine("Log file function: Retrieve user");
-            writer.WriteLine("Account: " + dataInter.acct +
-                    " has searched for last name: " + dataInter.lname
-                    + " and found a result for: " + dataInter.fname + " " +
-                    dataInter.lname);
-            writer.Close();
+            try
+            {
+                writer = new StreamWriter(path, append: true);
+                writer.WriteLine("Log file function: Retrieve user");
+                writer.WriteLine("Account: " + dataInter.acct +
+                        " has searched for last name: " + dataInter.lname
+                        + " and found a result for: " + dataInter.fname + " " +
+                        dataInter.lname);
+                writer.Close();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Unable to log data");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Unable to log data");
+            }
         }
 
         public void updateAccount(DataIntermed dataInter)
         {
-            writer = new StreamWriter(path, append: true);
-            writer.WriteLine("Log file function: Retrieve user");
-            writer.WriteLine("User " + dataInter.fname + " " + dataInter.lname + " at index: " + dataInter.index + " has been updated.");
-            writer.Close();
+            try
+            {
+                writer = new StreamWriter(path, append: true);
+                writer.WriteLine("Log file function: Retrieve user");
+                writer.WriteLine("User " + dataInter.fname + " " + dataInter.lname + " at index: " + dataInter.index + " has been updated.");
+                writer.Close();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Unable to log data");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Unable to log data");
+            }
         }
 
         public void errorLogMessage(string errorMessage)
         {
-            writer = new StreamWriter(path, append: true);
-            writer.WriteLine("Error Occurred: " + errorMessage);
-            writer.Close();
+            try
+            {
+                writer = new StreamWriter(path, append: true);
+                writer.WriteLine("Error Occurred: " + errorMessage);
+                writer.Close();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Unable to log error");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Unable to log error");
+            }
         }
 
         public void noResultFound(string lname)
         {
-            writer = new StreamWriter(path, append: true);
-            writer.WriteLine("Log file function: Search by lastname. ");
-            writer.WriteLine("No results for last name: " + lname +  "when searched");
-            writer.Close();
+            try
+            {
+                writer = new StreamWriter(path, append: true);
+                writer.WriteLine("Log file function: Search by lastname. ");
+                writer.WriteLine("No results for last name: " + lname + "when searched");
+                writer.Close();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Unable to log data");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Unable to log data");
+            }
         }
 
         public void logUrlChange(string urlText)
         {
-            writer = new StreamWriter(path, append: true);
-            writer.WriteLine("Log file function: Change of URL");
-            writer.WriteLine("Base URL was successfully changed to: " + urlText);
-            writer.Close();
+            try
+            {
+                writer = new StreamWriter(path, append: true);
+                writer.WriteLine("Log file function: Change of URL");
+                writer.WriteLine("Base URL was successfully changed to: " + urlText);
+                writer.Close();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Unable to log data");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Unable to log data");
+            }
         }
 
         public void setPath(string inPath)
@@ -87,10 +175,21 @@ namespace Bis_GUI
         }
         public void logTimerEnd()
         {
-            writer = new StreamWriter(path, append: true);
-            writer.WriteLine("Log file function: Bank transactions processed");
-            writer.WriteLine("2 Minutes passed. Bank Transactions processed succesfully");
-            writer.Close();
+            try
+            {
+                writer = new StreamWriter(path, append: true);
+                writer.WriteLine("Log file function: Bank transactions processed");
+                writer.WriteLine("2 Minutes passed. Bank Transactions processed succesfully");
+                writer.Close();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Unable to log data");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Unable to log data");
+            }
         }
     }
 }
