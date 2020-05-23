b9661108dcff8f47001a420fa28d571fa6675fae
aaron <aaron.gangers@gmail.com>
2020-05-23 11:37:55 +0800

    Tutorial 7 logging and threading complete successfully

--- Tutorials 6-9/Tutorial 7-8/Tutorial 7 Miner/Controllers/MinerController.cs
+++ Tutorials 6-9/Tutorial 7-8/Tutorial 7 Miner/Controllers/MinerController.cs
@@ -20,53 +20,60 @@ namespace Tutorial_7_Miner.Controllers
         [HttpPost]
         public async Task<bool> AddTransaction(uint walletIdFrom, uint walletIdTo, float amount)
         {
-            LogBlockchain log = new LogBlockchain();
-            log.logData("Adding new block to blockchain");
-            log.logData("Wallet ID from = " + walletIdFrom);
-            log.logData("Wallet ID to = " + walletIdTo);
-            log.logData("Transaction amount = " + amount);
+            bool isValidBlock = false;
             RestClient client = new RestClient("https://localhost:44353/");
-            string prevBlockHash;
-            RestRequest BlockchainRequest = new RestRequest("api/Server/GetBlockchain");
-            IRestResponse BlockchainResponse = await client.ExecuteGetAsync(BlockchainRequest);
-            List<Block> blockchain = JsonConvert.DeserializeObject<List<Block>>(BlockchainResponse.Content);
-            RestRequest IncrementRequest = new RestRequest("api/Server/Increment");
-            client.Put(IncrementRequest);
-            RestRequest OffsetRequest = new RestRequest("api/Server/GetOffset");
-            IRestResponse OffsetResponse = await client.ExecuteGetAsync(OffsetRequest);
-            uint offset = JsonConvert.DeserializeObject<uint>(OffsetResponse.Content);
-            log.logData("Offset = " + offset);
-            prevBlockHash = blockchain.Last().blockHash;
-            log.logData("Previous block hash = " + prevBlockHash);
-            SHA256 sha256 = SHA256.Create();
-            string blockString = walletIdFrom.ToString() + walletIdTo.ToString() + amount.ToString() + offset + prevBlockHash;
-            byte[] blockBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(blockString));
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < blockBytes.Length; i++)
+            RestRequest BalanceRequest = new RestRequest("api/Server/GetBalance/" + walletIdFrom.ToString());
+            IRestResponse BalanceResponse = await client.ExecuteGetAsync(BalanceRequest);
+            float balance = JsonConvert.DeserializeObject<float>(BalanceResponse.Content);
+            if (amount > 0 && walletIdFrom >= 0 && walletIdTo >= 0 && amount <= balance)
             {
-                builder.Append(blockBytes[i].ToString("x2"));
-            }
-            string hashString = "12345" + builder.ToString() + "54321";
-            log.logData("Block hash = " + hashString);
-            Block block = new Block(walletIdFrom, walletIdTo, amount, offset, prevBlockHash, hashString);
-            RestRequest ValidationRequest = new RestRequest("api/Server/ValidateBlock/");
-            ValidationRequest.AddJsonBody(block);
-            IRestResponse ValidationResponse = await client.ExecutePostAsync(ValidationRequest);
-            bool isValidBlock = JsonConvert.DeserializeObject<bool>(ValidationResponse.Content);
-            if(isValidBlock)
-            {
+                LogBlockchain log = new LogBlockchain();
+                log.logData("Adding new block to blockchain");
+                log.logData("Wallet ID from = " + walletIdFrom);
+                log.logData("Wallet ID to = " + walletIdTo);
+                log.logData("Transaction amount = " + amount);
+                string prevBlockHash;
+                RestRequest BlockchainRequest = new RestRequest("api/Server/GetBlockchain");
+                IRestResponse BlockchainResponse = await client.ExecuteGetAsync(BlockchainRequest);
+                List<Block> blockchain = JsonConvert.DeserializeObject<List<Block>>(BlockchainResponse.Content);
+                RestRequest IncrementRequest = new RestRequest("api/Server/Increment");
+                client.Put(IncrementRequest);
+                RestRequest OffsetRequest = new RestRequest("api/Server/GetOffset");
+                IRestResponse OffsetResponse = await client.ExecuteGetAsync(OffsetRequest);
+                uint offset = JsonConvert.DeserializeObject<uint>(OffsetResponse.Content);
+                log.logData("Offset = " + offset);
+                prevBlockHash = blockchain.Last().blockHash;
+                log.logData("Previous block hash = " + prevBlockHash);
+                SHA256 sha256 = SHA256.Create();
+                string blockString = walletIdFrom.ToString() + walletIdTo.ToString() + amount.ToString() + offset + prevBlockHash;
+                byte[] blockBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(blockString));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < blockBytes.Length; i++)
+                {
+                    builder.Append(blockBytes[i].ToString("x2"));
+                }
+                string hashString = "12345" + builder.ToString() + "54321";
+                log.logData("Block hash = " + hashString);
+                Block block = new Block(walletIdFrom, walletIdTo, amount, offset, prevBlockHash, hashString);
+                RestRequest ValidationRequest = new RestRequest("api/Server/ValidateBlock/");
+                ValidationRequest.AddJsonBody(block);
+                IRestResponse ValidationResponse = await client.ExecutePostAsync(ValidationRequest);
+                isValidBlock = JsonConvert.DeserializeObject<bool>(ValidationResponse.Content);
+                if (isValidBlock)
+                {
 
-                RestRequest AddBlockRequest = new RestRequest("api/Server/AddBlock/");
-                AddBlockRequest.AddJsonBody(block);
-                await client.ExecutePostAsync(AddBlockRequest);
-                log.logData("Block successfully added to Blockchain");
-                log.logData("Transaction successful");
+                    RestRequest AddBlockRequest = new RestRequest("api/Server/AddBlock/");
+                    AddBlockRequest.AddJsonBody(block);
+                    await client.ExecutePostAsync(AddBlockRequest);
+                    log.logData("Block successfully added to Blockchain");
+                    log.logData("Transaction successful");
+                }
+                else
+                {
+                    log.logData("Validation for block failed, Unable to add block");
+                }
+                log.logData(".................................................................");
             }
-            else
-            {
-                log.logData("Validation for block failed, Unable to add block");
-            }
-            log.logData(".................................................................");
             return isValidBlock;
         }
     }
