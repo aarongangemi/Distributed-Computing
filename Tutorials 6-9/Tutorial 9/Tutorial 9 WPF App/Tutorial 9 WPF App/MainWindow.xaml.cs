8f2c675cd7b26ef123233769a7a7a8ecadaadd66
aaron <aaron.gangers@gmail.com>
2020-05-29 11:35:56 +0800

    Tutorial 8 complete

--- Tutorials 6-9/Tutorial 9/Tutorial 9 WPF App/Tutorial 9 WPF App/MainWindow.xaml.cs
+++ Tutorials 6-9/Tutorial 9/Tutorial 9 WPF App/Tutorial 9 WPF App/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.ServiceModel;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using Tutorial_9_Blockchain_Library;
 
@@ -47,6 +48,8 @@ namespace Tutorial_9_WPF_App
             NetworkingThread.Start();
             JobsCompleted.Text = PortCounter.JobCounter.ToString();
             NoOfBlocks.Text = Blockchain.BlockChain.Count.ToString();
+            Thread JobListThread = new Thread(new ThreadStart(UpdateList));
+            JobListThread.Start();
         }
         private void BlockchainThread()
         {
@@ -130,83 +133,79 @@ namespace Tutorial_9_WPF_App
                     {
                         List<Transaction> transactionsList = TransactionStorage.TransactionQueue.OrderBy(key => key.PythonSrc).ToList();
                         uint offset = 0;
-                            string prevBlockHash;
-                            string hashString = "";
-                            SHA256 sha256 = SHA256.Create();
-                            prevBlockHash = Blockchain.BlockChain.Last().blockHash;
+                        string prevBlockHash;
+                        string hashString = "";
+                        SHA256 sha256 = SHA256.Create();
+                        prevBlockHash = Blockchain.BlockChain.Last().blockHash;
                         Block block = new Block(offset, prevBlockHash, hashString);
-                                    
+                        Dispatcher.Invoke(() =>
+                        {
+                            try
+                            {
+
+                                for (int j = 0; j < 5; j++)
+                                {
+                                    Transaction t = TransactionStorage.TransactionQueue.Dequeue();
+                                    if (!String.IsNullOrEmpty(t.PythonSrc))
                                     {
-                                        Dispatcher.Invoke(() =>
-                                        {
-                                            try
-                                            {
-                                               
-                                                for(int j = 0; j < 5; j++)
-                                                {
-                                                    Transaction t = TransactionStorage.TransactionQueue.Dequeue();
-                                                    if (!String.IsNullOrEmpty(t.PythonSrc))
-                                                    {
-                                                        byte[] decodedBytes = Convert.FromBase64String(t.PythonSrc);
-                                                        t.PythonSrc = Encoding.UTF8.GetString(decodedBytes);
-                                                        JobStatusLabel.Content = "Running Python Job";
-                                                        ScriptEngine engine = Python.CreateEngine();
-                                                        ScriptScope scope = engine.CreateScope();
-                                                        engine.Execute(t.PythonSrc, scope);
-                                                        dynamic PyFunc = scope.GetVariable("main");
-                                                        var result = PyFunc();
-                                                        PyResult.Content = result;
-                                                        t.PythonResult = PyResult.Content.ToString();
-                                                        Debug.WriteLine("Python Script executed successfully - Result = " + t.PythonResult);
-                                                        byte[] resultBytes = Encoding.UTF8.GetBytes(t.PythonResult);
-                                                        string base64String = Convert.ToBase64String(resultBytes);
-                                                        block.AddPythonTransaction(t.PythonSrc, t.PythonResult);
-                                                        JobStatusLabel.Content = "Python Job Complete";
-                                                        PortCounter.JobCounter++;
-                                                        JobsCompleted.Text = PortCounter.JobCounter.ToString();
-                                                    }
-                                                }
-                                                while (!hashString.StartsWith("12345"))
-                                                {
-                                                    offset++;
-                                                    string blockString = block.blockID + block.TransactionDetailsList + offset + prevBlockHash;
-                                                    byte[] textBytes = Encoding.UTF8.GetBytes(blockString);
-                                                    byte[] hashedData = sha256.ComputeHash(textBytes);
-                                                    hashString = BitConverter.ToUInt64(hashedData, 0).ToString();
-                                                }
-                                                block.blockHash = hashString;
-                                                block.blockOffset = offset;
-                                                if (Blockchain.ValidateBlock(block))
-                                                {
-                                                    Blockchain.AddBlock(block);
-                                                    Debug.WriteLine("Block successfully added");
-                                                    Debug.WriteLine("Block offset = " + offset);
-                                                    Debug.WriteLine("Block hash = " + block.blockHash);
-                                                    Debug.WriteLine("Block previous hash = " + prevBlockHash);
-                                                    Debug.WriteLine(Blockchain.BlockChain.Count +  "..............................................");
-                                                    Dispatcher.Invoke(() => { NoOfBlocks.Text = (Blockchain.BlockChain.Count).ToString(); });
-                                                }
-                                                else
-                                                {
-                                                    Debug.WriteLine("Validation for block failed, Trying again");
-                                                }
-                                            }
-                                            catch (SyntaxErrorException)
-                                            {
-                                                MessageBox.Show("Invalid Python script, please ensure python body is valid and proper indentation is used for variables");
-                                            }
-                                            catch (UnboundNameException)
-                                            {
-                                                MessageBox.Show("Invalid variables found in python body, please try again");
-                                            }
-                                            catch (NullReferenceException)
-                                            {
-                                                MessageBox.Show("No return type found in python script, please return something");
-                                            }
-                                        });
+                                        byte[] decodedBytes = Convert.FromBase64String(t.PythonSrc);
+                                        t.PythonSrc = Encoding.UTF8.GetString(decodedBytes);
+                                        JobStatusLabel.Content = "Running Python Job";
+                                        ScriptEngine engine = Python.CreateEngine();
+                                        ScriptScope scope = engine.CreateScope();
+                                        engine.Execute(t.PythonSrc, scope);
+                                        dynamic PyFunc = scope.GetVariable("main");
+                                        var result = PyFunc();
+                                        PyResult.Content = result;
+                                        t.PythonResult = PyResult.Content.ToString();
+                                        Debug.WriteLine("Python Script executed successfully - Result = " + t.PythonResult);
+                                        byte[] resultBytes = Encoding.UTF8.GetBytes(t.PythonResult);
+                                        string base64String = Convert.ToBase64String(resultBytes);
+                                        block.AddPythonTransaction(t.PythonSrc, t.PythonResult);
+                                        JobStatusLabel.Content = "Python Job Complete";
+                                        PortCounter.JobCounter++;
+                                        JobsCompleted.Text = PortCounter.JobCounter.ToString();
                                     }
                                 }
-
+                                while (!hashString.StartsWith("12345"))
+                                {
+                                    offset++;
+                                    string blockString = block.blockID + block.TransactionDetailsList + offset + prevBlockHash;
+                                    byte[] textBytes = Encoding.UTF8.GetBytes(blockString);
+                                    byte[] hashedData = sha256.ComputeHash(textBytes);
+                                    hashString = BitConverter.ToUInt64(hashedData, 0).ToString();
+                                }
+                                block.blockHash = hashString;
+                                block.blockOffset = offset;
+                                if (Blockchain.ValidateBlock(block))
+                                {
+                                    Blockchain.AddBlock(block);
+                                    Debug.WriteLine("Block successfully added");
+                                    Debug.WriteLine("Block offset = " + offset);
+                                    Debug.WriteLine("Block hash = " + block.blockHash);
+                                    Debug.WriteLine("Block previous hash = " + prevBlockHash);
+                                    Debug.WriteLine(Blockchain.BlockChain.Count + "..............................................");
+                                    Dispatcher.Invoke(() => { NoOfBlocks.Text = (Blockchain.BlockChain.Count).ToString(); });
+                                }
+                                else
+                                {
+                                    Debug.WriteLine("Validation for block failed, Trying again");
+                                }
+                            }
+                            catch (SyntaxErrorException)
+                            {
+                                MessageBox.Show("Invalid Python script, please ensure python body is valid and proper indentation is used for variables");
+                            }
+                            catch (UnboundNameException)
+                            {
+                                MessageBox.Show("Invalid variables found in python body, please try again");
+                            }
+                            catch (NullReferenceException)
+                            {
+                                MessageBox.Show("No return type found in python script, please return something");
+                            }
+                        });
+                    }
                     for (int i = 0; i < listOfClients.Count; i++)
                     {
                         NetTcpBinding tcp = new NetTcpBinding();
@@ -277,11 +276,17 @@ namespace Tutorial_9_WPF_App
                 {
                     Debug.WriteLine("Dispatcher had to cancel, trying again");
                 }
-
+            }
+        }
+        private void UpdateList()
+        {
+            while(true)
+            {
+                Thread.Sleep(10000);
                 Dispatcher.Invoke(() =>
                 {
                     JobListBox.Items.Clear();
-                    foreach(Block block in Blockchain.BlockChain)
+                    foreach (Block block in Blockchain.BlockChain)
                     {
                         foreach (string[] jsonTransaction in JsonConvert.DeserializeObject<List<string[]>>(block.TransactionDetailsList))
                         {
@@ -289,7 +294,7 @@ namespace Tutorial_9_WPF_App
                             {
                                 if (Encoding.UTF8.GetString(Convert.FromBase64String(transaction.PythonSrc)) == jsonTransaction[0])
                                 {
-                                    JobListBox.Items.Add(jsonTransaction[1]);
+                                    JobListBox.Items.Add("Client "+ transaction.TransactionId + " submitted script with result: " + jsonTransaction[1]);
                                 }
                             }
                         }
