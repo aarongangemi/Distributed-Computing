6d681a77bc645417352cd920a9b32e9b2926e480
aaron <aaron.gangers@gmail.com>
2020-05-30 16:49:50 +0800

    All commenting completed for DC assignment

--- XSS/Models/MessageItems.cs
+++ XSS/Models/MessageItems.cs
@@ -5,6 +5,11 @@ using System.Web;
 
 namespace XSS.Models
 {
+    /// <summary>
+    /// Purpose: Store a message item object containing subject and message
+    /// Author: Aaron Gangemi
+    /// Date Modified: 30/05/2020
+    /// </summary>
     public class MessageItems
     {
         public string subject;
--- XSS_Application_Fixed/Controllers/WebController.cs
+++ XSS_Application_Fixed/Controllers/WebController.cs
@@ -11,22 +11,34 @@ using XSS.Models;
 
 namespace XSS.Controllers
 {
+    /// <summary>
+    /// Purpose: The web controller is used to store a list of messages that are display in the admin web page.
+    /// It contains validation that is used to prevent a persistent XSS attack.
+    /// Author: Aaron Gangemi
+    /// Date Modified: 30/05/2020
+    /// </summary>
     public class WebController : ApiController
     {
         private static List<MessageItems> messageList = new List<MessageItems>();
-        // GET api/<controller>
+        /// <summary>
+        /// Purpose: To validate the passed in message, and if validation is successful, then store the message in the list
+        /// </summary>
+        /// <param name="mItem"></param>
         [Route("api/Web/StoreMessage/")]
         [HttpPost]
         public void StoreMessage([FromBody] MessageItems mItem)
         {
             bool validString = true;
-            if(!string.IsNullOrEmpty(mItem.subject) || !string.IsNullOrEmpty(mItem.message))
+            // indicates if validation was successful
+            if(!string.IsNullOrEmpty(mItem.subject) && !string.IsNullOrEmpty(mItem.message))
             { 
+                // message and subject cannot be empty fields
                 for(int i = 0; i < mItem.subject.Length; i++)
                 {
                     if(!Char.IsLetterOrDigit(mItem.subject, i))
                     {
                         validString = false;
+                        // check each character of subject to check it is not a special character
                         break;
                     }
                 }
@@ -34,18 +46,22 @@ namespace XSS.Controllers
                 {
                     if(!Char.IsLetterOrDigit(mItem.message,j))
                     {
+                        // check each character of message to check if it is not a special character
                         validString = false;
                         break;
                     }
                 }
                 if(validString)
                 {
+                    // if both checks have passed, then it is not an XSS attack so it can be added to list
                     messageList.Add(mItem);
                 }
                 else
                 {
+                    // if validation fails, throw exception back to web page
                     var response = new HttpResponseMessage(HttpStatusCode.NotFound)
                     {
+                        // response to print
                         Content = new StringContent(string.Format("Subject and message cannot contain invalid characters")),
                         ReasonPhrase = "subject or message contains invalid characters"
                     };
@@ -54,6 +70,7 @@ namespace XSS.Controllers
             }
             else
             {
+                // if string is null or empty, throw exception message back to user
                 var response = new HttpResponseMessage(HttpStatusCode.NotFound)
                 {
                     Content = new StringContent(string.Format("Subject and message cannot be null or empty")),
@@ -63,6 +80,10 @@ namespace XSS.Controllers
             }
         }
 
+        /// <summary>
+        /// Purpose: to retrieve the list of messages for the admin
+        /// </summary>
+        /// <returns>Message list</returns>
         [Route("api/Web/GetMessages")]
         [HttpGet]
         public List<MessageItems> GetMessages()
